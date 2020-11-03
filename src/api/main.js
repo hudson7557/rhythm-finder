1bf946e7285c3f51fa942c4acffa993336563198
Marissa Castillo <mmcastillo22@gmail.com>
2020-11-02 19:06:01 -0800

    add login and signup pages

--- src/api/main.js
+++ src/api/main.js
@@ -8,7 +8,12 @@ router.route("/")
 
 router.route("/signup")
     .get(function(req, res) {
-        res.render("splash");
+        res.render("signup");
+    });
+
+router.route("/login")
+    .get(function(req, res) {
+        res.render("login");
     });
 
 module.exports = router;
